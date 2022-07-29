c159cce072e0b2e99e280c84320db18ae898d79c
Shubham Yadav <58941129+a5hubh@users.noreply.github.com>
2022-07-29 16:58:37 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -38,5 +38,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
-fruit_choice = streamlit.text_input('What fruit would you like add?')
-streamlit.write('The user entered ', fruit_choice)
+add_my_fruit = streamlit.text_input('What fruit would you like add?')
+streamlit.write('The user entered ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values('from streamlit')");
